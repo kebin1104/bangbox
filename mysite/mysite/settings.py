e41cdbb90ddcc85ffc095d6afbe324452dfca3a3
kebin1104 <kebin1104@nate.com>
2015-07-20 19:13:28 +0900

    er

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -107,4 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = "/home/ubuntu/bangbox/mysite/bangbox/templates/static"
 
+STATICFILES_DIRS = (
+    ('css', '/home/ubuntu/bangbox/mysite/bangbox/templates/static/css'))
+
 STATIC_URL = '/static/'
