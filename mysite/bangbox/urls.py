d88f90395cb3a880ef8b19c3436f425cf675549f
kebin1104 <kebin1104@nate.com>
2015-07-20 19:50:41 +0900

    qwe

--- mysite/bangbox/urls.py
+++ mysite/bangbox/urls.py
@@ -3,6 +3,4 @@ from bangbox import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^static/css/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': ROOT_PATH+'/static'}),
 )
