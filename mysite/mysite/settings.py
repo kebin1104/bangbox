aac923f521bad7fa36d43baca95f46982b6407b2
kebin1104 <kebin1104@nate.com>
2015-07-20 16:46:41 +0900

    css

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -106,4 +106,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/templates/static/'
+STATIC_URL = '/templates/static/css/'
