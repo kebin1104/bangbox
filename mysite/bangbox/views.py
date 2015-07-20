6e7e5d0c6f2e84e8ba3cfdfe577f527e909b0dd2
kebin1104 <kebin1104@nate.com>
2015-07-20 17:56:23 +0900

    qwe

--- mysite/bangbox/views.py
+++ mysite/bangbox/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.templates import RequestContext
 
 # Create your views here.
 
 def index(request):
-    return render('bangbox/index.html',
-                  context_instance=RequestContext(request))
+    return render(request, 'bangbox/index.html')
