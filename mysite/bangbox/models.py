d8af86f1cb034708daa7c6b64d261b972beeb669
kebin1104 <kebin1104@nate.com>
2015-07-20 14:52:56 +0900

    models

--- mysite/bangbox/models.py
+++ mysite/bangbox/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=15)
+    password = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.email
