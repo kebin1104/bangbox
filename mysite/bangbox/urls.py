51913ae281100e7930815337a1883f7faade4ccb
kebin1104 <kebin1104@nate.com>
2015-07-20 22:28:32 +0900

    ch

--- mysite/bangbox/urls.py
+++ mysite/bangbox/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^bang/', views.bang, name='bang'),
     url(r'^login/', views.login, name='login'),
+    url(r'^check/', views.check, name='check'),
 )
