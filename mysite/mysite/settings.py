ef21a6931cd0b6cde808994188e9c464b6b70964
kebin1104 <kebin1104@nate.com>
2015-07-20 20:25:35 +0900

    del

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -105,11 +105,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = "/home/ubuntu/bangbox/mysite/bangbox/templates/static"
-
-STATICFILES_DIRS = (
-    "/home/ubuntu/bangbox/mysite/bangbox/templates/static/css",
-    "/home/ubuntu/bangbox/mysite/bangbox/templates/static/img",
-    )
 
 STATIC_URL = '/static/'
