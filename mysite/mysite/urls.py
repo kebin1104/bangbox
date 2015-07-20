7aeab93e76ae375b7b1cc957faacf294832218a1
kebin1104 <kebin1104@nate.com>
2015-07-20 20:37:46 +0900

    css

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,4 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^bangbox/', include('bangbox.urls', namespace="bangbox")),
     url(r'^admin/', include(admin.site.urls)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
