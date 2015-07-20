2cb162ecb8b703c181d5022a14ca47db2e47cf5d
kebin1104 <kebin1104@nate.com>
2015-07-20 20:32:18 +0900

    css

--- mysite/bangbox/urls.py
+++ mysite/bangbox/urls.py
@@ -3,4 +3,5 @@ from bangbox import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^bang/', views.bang, name='bang'),
 )
--- mysite/bangbox/views.py
+++ mysite/bangbox/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'bangbox/index.html')
+
+def bang(request):
+    return render(request, 'bangbox/bang.html')
