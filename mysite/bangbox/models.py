8d76fa2cc120bb7f64702eb52fce1fd214a64ab2
kebin1104 <kebin1104@nate.com>
2015-07-21 13:43:54 +0900

    pass

--- mysite/bangbox/models.py
+++ mysite/bangbox/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Bangbox_user(models.Model):
+class Bangbox_user(models.Model, User):
     email = models.EmailField()
     name = models.CharField(max_length=15)
     password = models.CharField(max_length=30)
