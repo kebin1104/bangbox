1dca93dd71e80ab1af1f2fa14ac4c482c8a45e42
kebin1104 <kebin1104@nate.com>
2015-07-21 14:59:43 +0900

    q

--- mysite/bangbox/models.py
+++ mysite/bangbox/models.py
@@ -1,18 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Bangbox_user(models.Model, User):
-    email = models.EmailField()
+class Bangbox_user(models.Model):
+    email = models.EmailField(unique=True, null=False)
     name = models.CharField(max_length=15)
     password = models.CharField(max_length=30)
 
     def __unicode__(self):
         return self.email
-
-    def save(self, *args, **kwargs):
-
-        self.set_password(self.password)
-
-        super(Bangbox_user, self).save(*args, **kwargs)
