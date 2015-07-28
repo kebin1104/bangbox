dbc1dcb28e3f4db187a6d3de88ad1d8617aa35b9
kebin1104 <kebin1104@nate.com>
2015-07-28 19:09:41 +0900

    wwww

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bangbox',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
--- pystagram/pystagram/settings.py
+++ pystagram/pystagram/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photo',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +77,14 @@ WSGI_APPLICATION = 'pystagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bang',
+        'USER': 'root',
+        'PASSWORD': 'dbsksl04',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
