25e267816ddf537354c2d5fb55622c72887e6444
kebin1104 <kebin1104@nate.com>
2015-07-28 20:34:07 +0900

    qwe

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -26,7 +26,7 @@ router.register(r'events', views.eventViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^bangbox/', include('bangbox.urls', namespace="bangbox")),
     url(r'^admin/', include(admin.site.urls)),
 ]
