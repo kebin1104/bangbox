3105b81c35e1583762c8624ac33f6d3b94a191ad
kebin1104 <kebin1104@nate.com>
2015-07-31 16:40:06 +0900

    admin

--- mysite/bangbox/admin.py
+++ mysite/bangbox/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from bangbox.models import user, event
+from bangbox.models import user, event, category, event_date, event_time
 
 # Register your models here.
 
 admin.site.register(user)
 admin.site.register(event)
+admin.site.register(category)
+admin.site.register(event_date)
+admin.site.register(event_time)
