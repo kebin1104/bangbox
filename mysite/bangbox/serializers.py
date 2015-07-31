0ccfb65ef3b9e86025e062c4e5824c63dea1f3e1
kebin1104 <kebin1104@nate.com>
2015-07-31 17:32:55 +0900

    ww

--- mysite/bangbox/serializers.py
+++ mysite/bangbox/serializers.py
@@ -4,10 +4,18 @@ from rest_framework import serializers
 
 from bangbox.models import user, event, category, event_date, event_time
 
+class bangboxUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = user
+        fields = [
+            'id', 'email', 'name',
+        ]
+
 class bangboxEventSerializer(serializers.ModelSerializer):
     # user_id = models.ForeignKey(user)
     # title = models.CharField(null=False, max_length=26)
     # sub = models.CharField(null=False, max_length=50)
+    # category_id = models.ForeignKey('category')
     # article = models.TextField()
     # address = models.CharField(null=False, max_length=50)
     # imageURL = models.URLField(null=False)
@@ -17,4 +25,28 @@ class bangboxEventSerializer(serializers.ModelSerializer):
             'id', 'user_id', 'title', 'sub', 'article', 'address', 'imageURL',
         ]
         
-class bangbox
+class bangboxCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = category
+        fields = [
+            'id', 'name',
+        ]
+
+class bangboxEventDateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = event_date
+        fieldsa = [
+            'id', 'event_id', 'date',
+        ]
+
+class bangboxEventTimeSerializer(serializers.ModelSerializer):
+
+    # event_date_id = models.ForeignKey(event_date)
+    # time = models.TimeField()
+    # price = models.IntegerField(null=False)
+
+    class Meta:
+        model = event_time
+        fieldsa = [
+            'id', 'event_id', 'date',
+        ]
