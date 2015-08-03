9805bea31c631ea803c98c6445e4e114e2bdb5be
unboxbetaorg <kebin1104@nate.com>
2015-08-03 16:14:54 +0900

    asd

--- pystagram/photo/views.py
+++ pystagram/photo/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def single_photo(request):
-    return HttpResponse('파일을 보여드립니다')
+def single_photo(request, photo_id):
+    return HttpResponse('{0}파일을 보여드립니다'.format(photo_id))
 
