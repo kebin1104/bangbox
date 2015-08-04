2afee13ea68576a0b4e54b452c5ef7a9be4b82fe
unboxbetaorg <kebin1104@nate.com>
2015-08-04 19:14:47 +0900

    ww

--- pystagram/photo/views.py
+++ pystagram/photo/views.py
@@ -12,7 +12,7 @@ def single_photo(request, photo_id):
     photo = get_object_or_404(Photo, pk=photo_id)
 
     response_text = '<p>{photo_id}번...{photo_id}번 사진을 보여드립니다.</p>'
-    response_text += '<p>{photo_url}</p>'
+    response_text += '<p><img src="{photo_url}" /></p>'
 
     return HttpResponse(response_text.format(photo_id=photo_id, photo_url=photo.image_file.url))
 
