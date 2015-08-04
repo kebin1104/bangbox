c4844da73a462b8f544dfca41612c423ae99c609
unboxbetaorg <kebin1104@nate.com>
2015-08-04 18:59:59 +0900

    asdasdasd

--- pystagram/photo/views.py
+++ pystagram/photo/views.py
@@ -1,10 +1,18 @@
 # coding: utf-8
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Photo
 
 # Create your views here.
 
 def single_photo(request, photo_id):
-    return HttpResponse('{0}파일을 보여드립니다'.format(photo_id))
+
+    photo = get_object_or_404(Photo, pk=photo_id)
+
+    response_text = '<p>{photo_id}번...{photo_id}번 사진을 보여드립니다.</p>'
+    response_text += '<p>{photo_url}</p>'
+
+    return HttpResponse(response_text.format(photo_id=photo_id, photo_url=photo.image_file.url))
 
