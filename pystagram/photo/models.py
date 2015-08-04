192fb39d572d18dea8dd69915c84b000c6742a24
unboxbetaorg <kebin1104@nate.com>
2015-08-05 01:54:27 +0900

    ㄱㄱ

--- pystagram/photo/models.py
+++ pystagram/photo/models.py
@@ -5,8 +5,8 @@ from django.db import models
 # Create your models here.
 
 class Photo(models.Model):
-    image_file = models.ImageField(upload_to='static_files/uploaded/original/%Y/%m/%d')
-    filtered_image_file = models.ImageField(upload_to='static_files/uploaded/filtered/%Y/%m/%d')
+    image_file = models.ImageField(upload_to='%Y/%m/%d')
+    filtered_image_file = models.ImageField(upload_to='static_files/uploaded/%Y/%m/%d')
     description = models.TextField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
